94ce80af444c507670be54e12d59c0da24ffb5ef
Oro1st <79486412+Oro1st@users.noreply.github.com>
2021-03-03 20:25:29 -0600

    LCM JS doc

--- WDClass/W2DClass/lcm.js
+++ WDClass/W2DClass/lcm.js
@@ -1,11 +1,14 @@
-
-
-
+"use strict";
+/**
+ * 
+ * @param {number} num1 is a number which is used to compute with num2  
+ * @param {number} num2 is a number which is used to compute with num2
+ */
 
 let compute = function (num1, num2) {
     for (let i = 1; i <= num1 && i <= num2; i++) {
 
-        // check if is factor of both integers
+
         if (num1 % i == 0 && num2 % i == 0) {
             hcf = i;
         }
@@ -15,7 +18,7 @@ let compute = function (num1, num2) {
     let lcm = (num1 * num2) / hcf;
 
     console.log(`HCF of ${num1} and ${num2} is ${lcm}.`);
-}
+};
 
 let hcf;
 compute(8, 12);
